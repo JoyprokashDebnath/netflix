e4282c25ab929081c88b243f74e00e486fca8c85
./Vishnu.sh <143306862+JoyprokashDebnath@users.noreply.github.com>
2025-05-27 16:12:30 +0530

    Update clab.c

--- clab.c
+++ clab.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[10][10], m, n, i, j;
+    int A[10][10], B[10][10], C[10][10], m, n, i, j;
 
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &m, &n);
 
-    printf("Enter elements of the matrix:\n");
+    printf("Enter elements of first matrix:\n");
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
-            scanf("%d", &matrix[i][j]);
+            scanf("%d", &A[i][j]);
 
-    printf("The matrix is:\n");
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &B[i][j]);
+
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+
+    printf("Resultant matrix after addition:\n");
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++)
-            printf("%d ", matrix[i][j]);
+            printf("%d ", C[i][j]);
         printf("\n");
     }
 
